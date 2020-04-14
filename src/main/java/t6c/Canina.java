ef28fefea43e7971205b11e8ffb7eb13c6f90e9f
salvi205 <salvi205s@gmail.com>
2020-04-14 16:00:59 +0200

    Indicar los metodos polimorficos

--- src/main/java/t6c/Canina.java
+++ src/main/java/t6c/Canina.java
@@ -20,8 +20,6 @@ public class Canina extends Especie {
         this.habitat = habitat;
     }
 
-
-
     public String getHabitat() {
         return habitat;
     }
@@ -30,6 +28,7 @@ public class Canina extends Especie {
         this.habitat = habitat;
     }
 
+    //metodo polimórfico
     @Override
     public void presentacion() {
         super.presentacion();
@@ -40,6 +39,7 @@ public class Canina extends Especie {
         System.out.println("Me paso todo el dia ladrando");
     }
 
+    //metodo polimórfico
     @Override
     public void comunicacion() {
         System.out.println("Me comunico ladrando");
