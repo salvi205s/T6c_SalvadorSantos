a68945697014f5d5947e717693f67840e655a97e
salvi205 <salvi205s@gmail.com>
2020-04-14 13:27:10 +0200

    Clase animal, con constructor getters, setters, equals, y toString

---
+++ src/main/java/t6c/Animal.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t6c;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Salva
+ */
+public class Animal {
+
+private VertebradoInvertebrado vertebradoInvertebrado;
+
+    public Animal(VertebradoInvertebrado vertebradoInvertebrado) {
+        this.vertebradoInvertebrado = vertebradoInvertebrado;
+    }
+
+    public VertebradoInvertebrado getVertebradoInvertebrado() {
+        return vertebradoInvertebrado;
+    }
+
+    public void setVertebradoInvertebrado(VertebradoInvertebrado vertebradoInvertebrado) {
+        this.vertebradoInvertebrado = vertebradoInvertebrado;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 31 * hash + Objects.hashCode(this.vertebradoInvertebrado);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Animal other = (Animal) obj;
+        if (this.vertebradoInvertebrado != other.vertebradoInvertebrado) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "vertebradoInvertebrado=" + vertebradoInvertebrado + '}';
+    }
+
+   
+
+   
+
+}
