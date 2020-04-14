56162817447359eb18c843fd0eb122a53c0b470d
salvi205 <salvi205s@gmail.com>
2020-04-14 14:02:46 +0200

    Comparar superclases dentro del metodo equals

--- src/main/java/t6c/Canina.java
+++ src/main/java/t6c/Canina.java
@@ -45,6 +45,11 @@ public class Canina extends Especie {
 
     @Override
     public boolean equals(Object obj) {
+        // Compara ambos vehiculos antes de comparar
+        // los atributos propios de turismo
+        if (!super.equals(obj)) {
+            return false;
+        }
         if (this == obj) {
             return true;
         }
