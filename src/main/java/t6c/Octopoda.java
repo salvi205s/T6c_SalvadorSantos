0da947e82a645e706eba59aefa7ed43a0a3b5207
salvi205 <salvi205s@gmail.com>
2020-04-14 13:28:04 +0200

    Clase Octopoda, con constructor getters, setters, equals, y toString

---
+++ src/main/java/t6c/Octopoda.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package t6c;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Salva
+ */
+public class Octopoda extends Especie {
+
+    private String habitat;
+
+    public Octopoda(String habitat, VertebradoInvertebrado vertebradoInvertebrado) {
+        super(vertebradoInvertebrado);
+        this.habitat = habitat;
+    }
+
+    public String getHabitat() {
+        return habitat;
+    }
+
+    public void setHabitat(String habitat) {
+        this.habitat = habitat;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 53 * hash + Objects.hashCode(this.habitat);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Octopoda other = (Octopoda) obj;
+        if (!Objects.equals(this.habitat, other.habitat)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "habitat=" + habitat + '}';
+    }
+
+}
